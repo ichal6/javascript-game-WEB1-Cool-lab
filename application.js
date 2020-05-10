eb407f155fd9b69ece1729375a52c2dc16d9cd29
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-05-10 12:34:53 +0200

    Add stable version

--- application.js
+++ application.js
@@ -1,13 +1,72 @@
 var cross = "X";
 var circle = "O";
+var actual_user = "X";
+var gameState = ["", "", "", "", "", "", "", "", ""];
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
-var index = document.getElementById("index_0").getAttribute("index");
-
-console.log(index);
-
-document.getElementById("index_0").innerHTML = circle;
+function restartGame(){
+	document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
+	gameState = ["", "", "", "", "", "", "", "", ""];
+	actual_user = "X";
+}
 
 function clickCell(index){
 	var id = "index_" + index;
-	document.getElementById(id).innerHTML = cross;
+	if(document.getElementById(id).innerHTML == ""){
+		document.getElementById(id).innerHTML = actual_user;
+		gameState[parseInt(index)] = actual_user;
+		console.log(gameState);
+		handleResultValidation();
+		switchUser();
+	}
+	else
+	{
+		alert("This field was used.");
+	}
+}
+
+function switchUser(){
+	if(actual_user == cross){
+		actual_user = circle;
+	}
+	else{
+		actual_user = cross;
+	}
+}
+
+
+
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break
+        }
+    }
+	if (roundWon) {
+		winningMessage();
+        restartGame();
+        return;
+    }
+}
+
+function winningMessage(){
+	alert(actual_user + " has won!");
 }
